a8a7f96bc6999890f59eeb61e4fbff259e3f5132
Stefan Lachmann <stefanlachmann@hotmail.com>
2019-03-24 12:10:28 +0100

    added lecture

---
+++ build/precache-manifest.70a1f229331c52cbd31e5f6ff3a96587.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e3803a948856774e203",
+    "url": "/static/css/main.3fde579b.chunk.css"
+  },
+  {
+    "revision": "3e3803a948856774e203",
+    "url": "/static/js/main.48d68a3a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cc9bbdd6f768dd32a4c5",
+    "url": "/static/js/2.b4f4a70b.chunk.js"
+  },
+  {
+    "revision": "c015b380de09fa502b5a8d0b124b6273",
+    "url": "/index.html"
+  }
+];
